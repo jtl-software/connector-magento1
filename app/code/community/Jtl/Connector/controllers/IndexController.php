8ea263604fa332a0e70a50933e4407e45c962ad1
Christian Spoo <christian.spoo@jtl-software.com>
2015-04-02 11:47:41 +0200

    Tidy up a bit

--- app/code/community/Jtl/Connector/controllers/IndexController.php
+++ app/code/community/Jtl/Connector/controllers/IndexController.php
@@ -9,8 +9,6 @@ class Jtl_Connector_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
-        // $authToken = Mage::helper('core')->decrypt(Mage::getStoreConfig('jtl_connector/general/auth_token'));
-
         defined('CONNECTOR_DIR') || define("CONNECTOR_DIR", __DIR__ . '/../');
         include (CONNECTOR_DIR . "/src/bootstrap.php");
     }    
