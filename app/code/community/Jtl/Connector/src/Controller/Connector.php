d60d947618ff8c60593067c3d430dc0160fb653a
Christian Spoo <christian.spoo@jtl-software.com>
2015-05-18 09:25:34 +0200

    Bump version to 1.0.1.0

--- app/code/community/Jtl/Connector/src/Controller/Connector.php
+++ app/code/community/Jtl/Connector/src/Controller/Connector.php
@@ -89,7 +89,7 @@ class Connector extends AbstractController
         $action->setHandled(true);
 
         $identification = new ConnectorIdentification();
-        $identification->setEndpointVersion('0.0.1')
+        $identification->setEndpointVersion('1.0.1.0')
             ->setPlatformName('Magento')
             ->setPlatformVersion(\Mage::getVersion());
 
