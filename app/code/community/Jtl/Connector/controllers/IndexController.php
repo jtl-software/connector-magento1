9d09d2106ca4f63953d3fe424b958150b3ddb219
Christian Spoo <christian.spoo@jtl-software.com>
2015-10-29 10:56:59 +0100

    Move JTL-Connector logs to var/log/jtlconnector

--- app/code/community/Jtl/Connector/controllers/IndexController.php
+++ app/code/community/Jtl/Connector/controllers/IndexController.php
@@ -10,7 +10,7 @@ class Jtl_Connector_IndexController extends Mage_Core_Controller_Front_Action
     public function indexAction()
     {
         defined('CONNECTOR_DIR') || define('CONNECTOR_DIR', __DIR__ . '/../');
-        defined('LOG_DIR') || define('LOG_DIR', Mage::getBaseDir('var') . '/jtlconnector/');
+        defined('LOG_DIR') || define('LOG_DIR', Mage::getBaseDir('var') . '/log/jtlconnector/');
         if (!file_exists(LOG_DIR)) {
             mkdir(LOG_DIR);
         }
