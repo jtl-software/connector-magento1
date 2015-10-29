2d1b2579941c07e9e5f464363bda691bcfc62e12
Christian Spoo <christian.spoo@jtl-software.com>
2015-10-29 10:28:02 +0100

    Put JTL-Connector logs into Magento's log directory

--- app/code/community/Jtl/Connector/controllers/IndexController.php
+++ app/code/community/Jtl/Connector/controllers/IndexController.php
@@ -9,7 +9,12 @@ class Jtl_Connector_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
-        defined('CONNECTOR_DIR') || define("CONNECTOR_DIR", __DIR__ . '/../');
+        defined('CONNECTOR_DIR') || define('CONNECTOR_DIR', __DIR__ . '/../');
+        defined('LOG_DIR') || define('LOG_DIR', Mage::getBaseDir('var') . '/jtlconnector/');
+        if (!file_exists(LOG_DIR)) {
+            mkdir(LOG_DIR);
+        }
+
         include (CONNECTOR_DIR . "/src/bootstrap.php");
-    }    
+    }
 }
