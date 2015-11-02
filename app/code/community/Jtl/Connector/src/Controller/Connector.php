0ae9bdef7bf0be7c96799a92a38e32fdd95bc064
Christian Spoo <christian.spoo@jtl-software.com>
2015-11-02 10:33:51 +0100

    Bump version to 1.4.0.2

--- app/code/community/Jtl/Connector/src/Controller/Connector.php
+++ app/code/community/Jtl/Connector/src/Controller/Connector.php
@@ -112,7 +112,7 @@ class Connector extends AbstractController
             ->setUploadMaxFilesize($returnMegaBytes(ini_get('upload_max_filesize')));
 
         $identification = new ConnectorIdentification();
-        $identification->setEndpointVersion('1.4.0.1')
+        $identification->setEndpointVersion('1.4.0.2')
             ->setPlatformName('Magento')
             ->setPlatformVersion(\Mage::getVersion())
             ->setProtocolVersion(Application()->getProtocolVersion())
