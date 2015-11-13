158ee0150637fd9ec74aab1ea09ca7d22419d0ab
Christian Spoo <christian.spoo@jtl-software.com>
2015-11-13 11:35:59 +0100

    Bump version to 1.4.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
-1.4.2.0 (unreleased)
+1.4.3.0 (unreleased)
 --------------------
+
+1.4.2.0
+-------
 - Improve the performance of image.statistic and image.pull
 - Completely rewritten image import process
 - Fix SQL query in payment.pull
--- app/code/community/Jtl/Connector/src/Controller/Connector.php
+++ app/code/community/Jtl/Connector/src/Controller/Connector.php
@@ -112,7 +112,7 @@ class Connector extends AbstractController
             ->setUploadMaxFilesize($returnMegaBytes(ini_get('upload_max_filesize')));
 
         $identification = new ConnectorIdentification();
-        $identification->setEndpointVersion('1.4.1.0')
+        $identification->setEndpointVersion('1.4.2.0')
             ->setPlatformName('Magento')
             ->setPlatformVersion(\Mage::getVersion())
             ->setProtocolVersion(Application()->getProtocolVersion())
