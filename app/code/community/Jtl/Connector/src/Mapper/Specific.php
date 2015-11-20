1617391e64c7e09239834f12b1560b54ac58077a
Christian Spoo <christian.spoo@jtl-software.com>
2015-11-20 10:46:38 +0100

    Cleanup unused code

--- app/code/community/Jtl/Connector/src/Mapper/Specific.php
+++ app/code/community/Jtl/Connector/src/Mapper/Specific.php
@@ -267,50 +267,7 @@ class Specific
             'value' => $options
         ));
         $attrModel->setData('default_value', $defaultValue);
-//        die(var_dump($attrModel));
         $attrModel->save();
-
-//        foreach ($specific->getValues() as $specificValue) {
-//            $defaultSpecificValueI18n = ArrayTools::filterOneByLanguage($specificValue->getI18ns(), $defaultLanguageIso);
-//            if ($defaultSpecificValueI18n === null)
-//                $defaultSpecificValueI18n = reset($specificValue->getI18ns());
-//            $matches = array_filter($values, function ($value) use ($defaultSpecificValueI18n) {
-//                return ($value['label'] === $defaultSpecificValueI18n->getValue());
-//            });
-//
-//            // Value found
-//            if ($matches)
-//                continue;
-//
-//            Logger::write(sprintf('value "%s" not found', $specificValue->getId()->getHost()), Logger::DEBUG);
-//
-//            $attribute_model = \Mage::getModel('eav/entity_attribute');
-//            $attribute_options_model = \Mage::getModel('eav/entity_attribute_source_table');
-//
-//            $attribute_table = $attribute_options_model->setAttribute($attribute);
-//            $options = $attribute_options_model->getAllOptions(false);
-//            Logger::write(var_export($options, true), Logger::DEBUG);
-//
-//            $stores = Magento::getInstance()->getStoreMapping();
-//            $newAttributeValue = array('option' => array());
-//            $newAttributeValue['option'] = array(
-//                \Mage_Core_Model_App::ADMIN_STORE_ID => $defaultSpecificValueI18n->getValue()
-//            );
-//            foreach ($stores as $locale => $storeId) {
-//                $specificValueI18n = ArrayTools::filterOneByLanguage($specificValue->getI18ns(), LocaleMapper::localeToLanguageIso($locale));
-//                if ($specificValueI18n === null) {
-//                    $i18ns = $specificValue->getI18ns();
-//                    $specificValueI18n = reset($i18ns);
-//                }
-//
-//                $newAttributeValue['option'][$storeId] = $specificValueI18n->getValue();
-//            }
-//            $result = array('value' => $newAttributeValue);
-//            $attribute->setData('option', $result);
-//            die(var_dump($attribute));
-//            $attribute->save();
-//
-//        }
     }
 
     public function update(ConnectorSpecific $specific)
